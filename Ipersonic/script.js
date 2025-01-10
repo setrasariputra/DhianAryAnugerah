e0557fa2b17f9bc062cdb68a50f83b802fd984a3
Wisnu K. Ardianto <setrasariputra@gmail.com>
2025-01-10 22:33:11 +0700

    Implement footerContainer function

--- Ipersonic/script.js
+++ Ipersonic/script.js
@@ -4,11 +4,29 @@ class PersonalityQuiz {
     }
 
     init() {
-        console.log('tes init console.log');
+        this.displayDropdownTopMenu();
+        this.displayFooter();
+        this.displayQuizOptions();
     }
 
-    showAlert() {
-        alert('tes alert from method');
+    displayDropdownTopMenu() {
+
+    }
+
+    displayFooter() {
+        let footerHTML = '<p>© iPersonic <a href="#">Personality Test</a> <a href="#">Career Test</a> <a href="#">Persönlichkeitstest</a> <a href="#">Berufstest</a> <a href="#">Privacy Policy</a> <a href="#">Felicitas Heyne</a> <a href="#">Impressum</a> <a href="#">Help & Contact</a>   iPersonic supports <a href="#">Audiopedia Foundation</a></p>';
+        footerHTML += '<p>English | Deutsch | Español | Français | Português | 中文 | Русский | हिंदी | العربية | 日本語 | Bahasa Indonesia | Italiano | Svenska | Nederlands | עברית | Afrikaans | Bosanski | Català | Čeština | Hrvatski | Magyar | Română | Slovenski | Slovenský | Srpski | Български</p>';
+        
+        const footerContainer = document.querySelector('.footer-container');
+        footerContainer.innerHTML = footerHTML;
+    }
+
+    displayQuizOptions() {
+        
+    }
+
+    saveQuizAnswer() {
+
     }
 }
 
