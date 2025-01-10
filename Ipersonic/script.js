7b7de6528385caeceda5fa1260a037f087733e6b
Wisnu K. Ardianto <setrasariputra@gmail.com>
2025-01-10 22:12:34 +0700

    Create class PersonalityQuiz

--- Ipersonic/script.js
+++ Ipersonic/script.js
@@ -1,1 +1,15 @@
-console.log('hello JS');
+class PersonalityQuiz {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        console.log('tes init console.log');
+    }
+
+    showAlert() {
+        alert('tes alert from method');
+    }
+}
+
+new PersonalityQuiz();
